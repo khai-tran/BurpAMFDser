b2be6812677821c748674432dd0d3cc125437896
KTR <fanronaldo86@gmail.com>
2013-01-08 16:33:27 -0600

    Update with new Burp API (since 1.5.01)
    Executable is a single file now and can be loaded with Burp Extender.

--- src/burp/AMFUtilities.java
+++ src/burp/AMFUtilities.java
@@ -1,9 +1,10 @@
+package burp;
 import java.net.URL;
 import java.util.Arrays;
 
 import org.apache.jmeter.protocol.amf.util.AmfXmlConverter;
 
-public class Utilities {
+public class AMFUtilities {
 	static final String X_BURP_DESERIALIZED = "X-Burp: Deserialized";
 	static final String X_BURP_INITDESERIALIZED = "X-Burp: InitDeserialized";
 
@@ -15,37 +16,25 @@ public class Utilities {
 	static String CONTENT_TYPE = "content-type: ";
 
 	public static byte[] getBody(byte[] message) {
+
 		String testStr = new String(message);
 		String[] strHeadersAndContent = testStr.split(DOUBLELINEBREAK);
 		byte[] reqBody = Arrays.copyOfRange(message, strHeadersAndContent[0].getBytes().length + 4, message.length);
 		return reqBody;
 	}
+	public static String getHeader(byte[] message) {
 
-	public static byte[] initDeserializeProxyItem(byte[] message) {
-		try {
-			String testStr = new String(message);
-			String[] strHeadersAndContent = testStr.split(DOUBLELINEBREAK);
-//			if (!strHeadersAndContent[0].toLowerCase().split(CONTENT_TYPE)[1].split(LINESEPARATOR)[0].equals(AMF_CONTENT_TYPE))
-//				return message;
-			String xml = AmfXmlConverter.convertAmfMessageToXml(getBody(message));
-			if (xml == null) {
-				return null;
-			}
-			String headerWithUpdatedLength = strHeadersAndContent[0].replaceAll("Content-Length: .*", "Content-Length: " + String.valueOf(xml.length()));
-			return (headerWithUpdatedLength + LINESEPARATOR + X_BURP_INITDESERIALIZED + LINESEPARATOR + LINESEPARATOR + xml).getBytes();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return message;
-		}
+		String testStr = new String(message);
+		String[] strHeadersAndContent = testStr.split(DOUBLELINEBREAK);
+		return strHeadersAndContent[0];
 	}
+
 
 	public static byte[] deserializeProxyItem(byte[] message) {
 		try {
 			String testStr = new String(message);
 			String[] strHeadersAndContent = testStr.split(DOUBLELINEBREAK);
-//			if (!strHeadersAndContent[0].toLowerCase().split(CONTENT_TYPE)[1].split(LINESEPARATOR)[0].equals(AMF_CONTENT_TYPE))
-//				return message;
-			String xml = AmfXmlConverter.convertAmfMessageToXml(getBody(message));
+			String xml = AmfXmlConverter.convertAmfMessageToXml(getBody(message), true);
 			if (xml == null) {
 				return null;
 			}
@@ -66,7 +55,7 @@ public class Utilities {
 			if (content == null)
 				return null;
 			String headerWithUpdatedLength = strHeadersAndContent[0].replaceAll("Content-Length: .*", "Content-Length: " + String.valueOf(content.length));
-			byte[] header = (headerWithUpdatedLength + LINESEPARATOR + X_BURP_SERIALIZED + LINESEPARATOR + LINESEPARATOR).getBytes();
+			byte[] header = (headerWithUpdatedLength  + LINESEPARATOR + LINESEPARATOR).getBytes();
 			byte[] retArray = new byte[header.length + content.length];
 			System.arraycopy(header, 0, retArray, 0, header.length);
 			System.arraycopy(content, 0, retArray, header.length, content.length);
@@ -78,6 +67,26 @@ public class Utilities {
 
 	}
 
+	public static byte[] serializeProxyItem(byte[] message, byte[] body) {
+
+		try {
+			String strMessage = new String(message);
+			String[] strHeadersAndContent = strMessage.split(DOUBLELINEBREAK);
+			byte[] content = AmfXmlConverter.convertXmlToAmfMessage(new String(body));
+			if (content == null)
+				return null;
+			String headerWithUpdatedLength = strHeadersAndContent[0].replaceAll("Content-Length: .*", "Content-Length: " + String.valueOf(content.length));
+			byte[] header = (headerWithUpdatedLength + LINESEPARATOR + LINESEPARATOR).getBytes();
+			byte[] retArray = new byte[header.length + content.length];
+			System.arraycopy(header, 0, retArray, 0, header.length);
+			System.arraycopy(content, 0, retArray, header.length, content.length);
+			return retArray;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return message;
+		}
+
+	}
 	public static void print(byte[] header) {
 		System.out.println(new String(header));
 	}
